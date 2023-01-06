502a839c11b104994b0008fb8159bbb23397643c
wildbornean <wbornean@gmail.com>
2023-01-07 00:12:25 +0700

    first commit

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section> 
+    <h1><?php echo $judul ?></h1>
+<h4>Nama</h4> 
+<ul type="disc"> 
+    <li>Nama Depan : Alif</li> 
+    <li>Nama Belakang : BA Ta</li> 
+</ul> 
+<br> 
+<h4>Alamat</h4> 
+<ul type="none"> 
+    <li> Jalan Ringroad Barat, Gamping, Sleman</li> 
+</ul> 
+<h4>Tempat Lahir</h4> 
+<ul type="none"> 
+    <li>Sleman</li>
+</ul> 
+<h4>Olah Raga Favorit</h4> 
+<ul type="square"> 
+    <li>Bulutangkis</li> 
+    <li>Catur</li> 
+</ul> 
+</section>
